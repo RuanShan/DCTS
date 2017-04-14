e3c3e77e8b9d099dcc8d0f00ff947e57acf8889b
qinghe <areq22@gmail.com>
2017-04-14 22:45:43 +0800

    开发功能维护景点信息

--- DCTS/UI/ScenicsControl.cs
+++ DCTS/UI/ScenicsControl.cs
@@ -7,14 +7,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using DCTS.DB;
+using DCTS.Uti;
+using DCTS.Bus;
 
 namespace DCTS.UI
 {
+    
     public partial class ScenicsControl : UserControl
     {
         public ScenicsControl()
         {
             InitializeComponent();
+            dataGridView.AutoGenerateColumns = false;
         }
 
         public void BeginActive()
@@ -27,13 +32,19 @@ namespace DCTS.UI
             int offset = 0;
             int pageSize = 5000;
             var ctx = this.entityDataSource1.DbContext as DctsEntities;
-            //using (var ctx = new DctsEntities())
-            {
+             
 
-                var query = ctx.Scenics.OrderBy(o => o.id).Skip(offset).Take(pageSize);
-                var list = this.entityDataSource1.CreateView(query);
-                this.dataGridView.DataSource = list;
-            }
+            var query = ctx.Scenics.OrderBy(o => o.id).Skip(offset).Take(pageSize);
+            var list = this.entityDataSource1.CreateView(query);
+            this.dataGridView.DataSource = list;
+            
+            // 初始化查询条件
+            var nationList = DCTS.DB.GlobalCache.NationList;
+            var nations = nationList.Select(o => new MockEntity { Id = o.id, ShortName= o.code ,FullName = o.title }).ToList();
+            nations.Insert(0, new MockEntity { Id = 0, ShortName = "", FullName = "所有" });
+            this.nationComboBox.DisplayMember = "FullName";
+            this.nationComboBox.ValueMember = "ShortName";
+            this.nationComboBox.DataSource = nations;
 
         }
 
@@ -43,6 +54,44 @@ namespace DCTS.UI
             if (form.ShowDialog() == System.Windows.Forms.DialogResult.Yes)
             {
                 InitializeDataGridView();
+            }
+        }
+
+        private void nationComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string code = nationComboBox.SelectedValue.ToString();
+
+            var cityList = DCTS.DB.GlobalCache.CityList;
+            cityList = cityList.Where(o => o.nationCode == code).ToList();
+            var cities = cityList.Select(o => new MockEntity { Id = o.id,  FullName = o.title }).ToList();
+            cities.Insert(0, new MockEntity { Id = 0,  FullName = "所有" });
+
+            this.cityComboBox.DisplayMember = "FullName";
+            this.cityComboBox.ValueMember = "Id";
+            this.cityComboBox.DataSource = cities;
+        }
+
+        private void dataGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            DataGridViewColumn column = dataGridView.Columns[e.ColumnIndex];
+
+            if (column == editColumn1)
+            {
+                MessageBox.Show("edit");
+            }
+            else if (column == deleteColumn)
+            {
+                var row = dataGridView.Rows[e.RowIndex];
+                var model = row.DataBoundItem as Scenic;
+                string msg = string.Format("确定删除景点<{0}>？", model.title);
+
+                if (MessageHelper.DeleteConfirm(msg))
+                {
+
+                    ScenicDbHelper.Delete(model.id);
+
+                    BeginActive();
+                }
             }
         }
     }
