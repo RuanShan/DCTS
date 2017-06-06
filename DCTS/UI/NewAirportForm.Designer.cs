ab0da80349238c41241864cfc03be41a3c86e183
lewishui <lewis.hui@qq.com>
2017-06-06 23:25:44 +0800

    060623
    up

--- DCTS/UI/NewAirportForm.Designer.cs
+++ DCTS/UI/NewAirportForm.Designer.cs
@@ -142,6 +142,7 @@
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(31, 13);
             this.label1.TabIndex = 0;
+            this.label1.Text = "机场";
             // 
             // entityDataSource1
             // 
