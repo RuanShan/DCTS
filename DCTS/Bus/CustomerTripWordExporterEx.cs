45ab5f2f8b57f6f5030de2b3578fec4e1cb08556
qinghe <areq22@gmail.com>
2017-06-06 14:16:05 +0800

    编辑tripday

---
+++ DCTS/Bus/CustomerTripWordExporterEx.cs
@@ -0,0 +1,399 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using DocumentFormat.OpenXml.Packaging;
+using DCTS.DB;
+using System.IO;
+using WindowsBitmap = System.Drawing.Bitmap;
+using DocumentFormat.OpenXml.Wordprocessing;
+
+namespace DCTS.Bus
+{
+    //http://www.cnblogs.com/kesalin/archive/2012/04/18/open_xml_word.html
+    class CustomerTripWordExporterEx
+    {
+        long TripId {get; set;}
+
+        Trip trip;
+        List<TripDay> days;
+        List<DayLocation> dayLocations;
+        List<WordprocessingDocument> filledTemplates;
+        List<MemoryStream> filledTemplateStreams = new List<MemoryStream>();
+
+        public CustomerTripWordExporterEx(long trip_id)
+        {
+            TripId = trip_id;
+
+            using (var ctx = new DctsEntities())
+            {
+                trip = ctx.Trips.Find(TripId);
+                days = trip.TripDays.ToList();
+                dayLocations = ctx.DayLocations.Include("TripDay").Include("ComboLocation").Where(o => o.trip_id == TripId).OrderBy(o => o.TripDay.day).ThenBy(o => o.position).ToList();
+            }
+
+            filledTemplates = new List<WordprocessingDocument>();
+            filledTemplateStreams = new List<MemoryStream>();
+        }
+
+
+
+        public bool ExportWord( )
+        {
+            var sources = new List<WordprocessingDocument>();
+
+            using (var db = new DctsEntities())
+            {
+                var tripSumaryTemplate = WordprocessingDocument.Open(LocationTemplate.TripSumaryPath,false);
+                var daySumaryTemplate = WordprocessingDocument.Open(LocationTemplate.DaySumaryPath, false);
+
+                //copy templates/base as template
+                string layout = LocationTemplate.LayoutRelativePath;
+                string tripPath = EntityPathConfig.TripWordFilePath(this.TripId);
+                File.Copy(layout, tripPath,true);
+
+                using (WordprocessingDocument document = WordprocessingDocument.Open(tripPath, true) )
+                {
+                    // 处理路书行程概要
+
+                    HandleTripSummary(tripSumaryTemplate);
+
+                    foreach (var day in this.days)
+                    {
+                        HandleDaySummary(daySumaryTemplate, day);
+                        //处理每天行程概要
+						var daylocations = dayLocations.Where(o => o.day_id == day.id).ToList();
+
+                        foreach (var dl in daylocations)
+                        {
+
+							HandleLocation(  dl );
+                        }
+                    }
+                   //合并文档
+                    WordTemplateHelper.MergeDoc(filledTemplateStreams, document);
+                    document.Save();//保存
+                }
+
+                var trip = db.Trips.Find(TripId);
+
+                trip.word_created_at = DateTime.Now;
+
+                daySumaryTemplate.Close();
+                tripSumaryTemplate.Close();
+                db.SaveChanges();
+            }
+            return true;
+        }
+
+
+        // params
+        //   doc: 添加行程概览到路书
+        public void HandleTripSummary(WordprocessingDocument template)
+        {
+            string[] tripKeys = { "days" };
+            string[] specKeys = { "trip_startend", "trip_dayLocations" };
+            var stream = new MemoryStream();
+            var tripSumary = template.Clone(stream, true) as WordprocessingDocument;
+
+            WordTemplateHelper.ReplaceTextWithProperty<Trip>(tripSumary, tripKeys, trip);
+
+            foreach (string key in specKeys)
+            {
+                string wrappedKey = "%" + key + "%";
+                if (key == "trip_startend")
+                {
+                    WordTemplateHelper.ReplaceText(tripSumary, wrappedKey, WordTemplateHelper.DisplayStartAndEndTime(trip));
+                }
+            }
+
+            var mainDoc = tripSumary.MainDocumentPart.Document;
+            //look for one specific table here
+            Table dayTable = mainDoc.Body.Descendants<Table>().FirstOrDefault();
+            if (dayTable != null)
+            {
+                //Row 0 and 1 are Headers
+                //Row 2 is pattern
+                var rows = dayTable.Elements<TableRow>().ToList();
+                if (rows.Count > 2)
+                {
+                    //get the Pattern row for duplication
+                    var rowPattern = rows.Last();
+                    //
+                    for (int i = 0; i < this.days.Count; i++)
+                    {
+                        var day = days[i];
+                        var date = trip.start_at.GetValueOrDefault().AddDays(day.day - 1);
+                        var locations = this.dayLocations.Where(o => o.day_id == day.id).ToList();
+                        //InsertRow performs a copy, so we get markup in new line ready for replacements
+
+                        var rowCopy = rowPattern.CloneNode(true) as TableRow;
+
+                        WordTemplateHelper.ReplaceText<TableRow>(rowCopy, "%day_day%", day.day.ToString());
+                        WordTemplateHelper.ReplaceText<TableRow>(rowCopy, "%day_date%", String.Format("{0:yyyyMMdd}", date));
+                        WordTemplateHelper.ReplaceText<TableRow>(rowCopy, "%day_weekday%",  String.Format("{0:ddd}", date));
+                        WordTemplateHelper.ReplaceText<TableRow>(rowCopy, "%day_cities%", WordTemplateHelper.DisplayDayCities(locations));
+
+                        var cell = rowCopy.Elements<TableCell>().ElementAt(3);
+                        var runPattern = cell.Descendants<Run>().Last();
+                        var titles = locations.Select(o => o.ComboLocation.title).Distinct().ToArray();
+                        for (int j = 0; j < titles.Count(); j++)
+                        {
+                            var t = titles[j];
+                            var runCopy = runPattern.CloneNode(true) as Run;
+                            WordTemplateHelper.ReplaceText<Run>(runCopy, "%day_location%", t);
+                            if (j > 0)
+                            {
+                                runPattern.InsertBeforeSelf(new Break());
+                            }
+                            runPattern.InsertBeforeSelf(runCopy);                            
+                        }
+                        runPattern.Remove();
+                        WordTemplateHelper.ReplaceText<TableRow>(rowCopy, "%day_hotel%", WordTemplateHelper.DisplayDayHotel(locations));
+
+                        dayTable.AppendChild(rowCopy);
+                    }
+
+                    //pattern row is at the end now, can be removed from table
+                    rowPattern.Remove();
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("\tError, couldn't find table with caption ORDER_TABLE in document");
+            }
+            tripSumary.Save();
+
+            this.filledTemplates.Add(tripSumary);
+            this.filledTemplateStreams.Add(stream);
+            
+        }
+
+        public void HandleDaySummary(WordprocessingDocument template, TripDay day)
+        {
+            string[] keys = { "day" };
+            string[] specKeys = { "day_date", "day_locations", "day_lweekday" };
+            var stream = new MemoryStream();
+            var sumary = template.Clone(stream, true) as WordprocessingDocument;
+			//替换一般属性
+            WordTemplateHelper.ReplaceTextWithProperty<TripDay>(sumary, keys, day);
+			//替换特殊文本
+			var date = this.trip.start_at.GetValueOrDefault().AddDays(day.day - 1);
+            var locations = this.dayLocations.Where(o => o.day_id == day.id).ToList();
+
+            foreach (string key in specKeys)
+            {
+                string wrappedKey = "%" + key + "%";
+                if (key == "day_date")//2017年6月1日
+                {
+                    WordTemplateHelper.ReplaceText(sumary, wrappedKey, WordTemplateHelper.DisplayDate( date));
+                }
+                if (key == "day_lweekday")//星期四
+                {					
+                    WordTemplateHelper.ReplaceText(sumary, wrappedKey, WordTemplateHelper.DisplayLongWeekDay(date ));
+                }
+                if (key == "day_locations")//北京-阿姆斯特丹-罗马
+                {
+                    WordTemplateHelper.ReplaceText(sumary, wrappedKey, WordTemplateHelper.DisplayDayLocations(locations, "-"));
+                }
+            }
+            var mainDoc = sumary.MainDocumentPart.Document;
+
+			Table dayTable = mainDoc.Body.Descendants<Table>().LastOrDefault();
+            if (dayTable != null)
+            {
+                //Row 0 is pattern
+                var rows = dayTable.Elements<TableRow>().ToList();
+                if (rows.Count > 0)
+                {
+                    //get the Pattern row for duplication
+                    var rowPattern = rows.Last();
+                    for (int i = 0; i < locations.Count; i++)
+                    {
+                        var dl = locations[i];
+                        if (dl.ComboLocation.ltype == (int)ComboLocationEnum.Blank) 
+                        {
+                            continue;
+                        }
+						var start_at = dl.start_at.GetValueOrDefault();
+                        var schedule = ((dl.schedule != null) && (dl.schedule.Length > 0)) ? dl.schedule : dl.ComboLocation.title;
+
+                        var rowCopy = rowPattern.CloneNode(true) as TableRow;
+                        WordTemplateHelper.ReplaceText<TableRow>(rowCopy, "LocationStartAt", String.Format("{0:HH:mm}", start_at));
+                        WordTemplateHelper.ReplaceText<TableRow>(rowCopy, "LocationSchedule", schedule);
+                        dayTable.AppendChild(rowCopy);
+                    }
+                    rowPattern.Remove();
+                }
+            }
+            sumary.Save();
+
+            this.filledTemplates.Add(sumary);
+            this.filledTemplateStreams.Add(stream);
+        }
+
+
+        public void HandleLocation( DayLocation dayLocation)
+        {
+            string[] locationKeys = {"nation","city", "area", "title", "local_title", "address", "local_address", "latlng", "route", "contact", "tips",
+                              // 景点
+                                "open_at", "close_at", "ticket",
+                               //住宿
+                               "room", "dinner", "wifi", "parking", "reception", "kitchen",
+                               // 
+                               "recommended_dishes"
+                            };
+            string[] specKeys = { "openning_hours" };
+            var wrappedKeys = locationKeys.Select(o => "%" + o + "%").ToList();
+
+            var location = dayLocation.ComboLocation;
+            var locationType = (ComboLocationEnum)location.ltype;
+            string templatePath = LocationTemplate.GetPath(location);
+
+            if (templatePath.Length == 0)
+            {
+                return;
+            }
+
+            using (var template = WordprocessingDocument.Open(templatePath, false))
+            {
+
+                var type = location.GetType();
+                var stream = new MemoryStream();
+                var duplicated = template.Clone(stream, true) as WordprocessingDocument;
+                //https://stackoverflow.com/questions/19094388/openxml-replace-text-in-all-document
+
+                if (locationType != ComboLocationEnum.Blank)
+                {
+                    WordTemplateHelper.ReplaceTextWithProperty<ComboLocation>(duplicated, locationKeys, location);
+
+                    foreach (string key in specKeys)
+                    {
+                        string wrappedKey = "%" + key + "%";
+
+                        if (key == "openning_hours")
+                        {
+                            WordTemplateHelper.ReplaceText(duplicated, wrappedKey, ComboLoactionBusiness.DisplayOpeningHours(location));
+                        }
+                    }
+                    if (location.img.Length > 0)
+                    {
+                        var image = duplicated.MainDocumentPart.ImageParts.ElementAtOrDefault(0);
+                        //Image img = duplicated.Images[0];
+
+                        string path = EntityPathConfig.LocationImagePath(location);
+                        if (image != null && File.Exists(path))
+                        {
+                            var b = new WindowsBitmap(path);
+
+
+                            image.FeedData(File.Open(path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite));
+                            // Save this Bitmap back into the document using a Create\Write stream.
+                            //b.Save(img.GetStream(FileMode.Create, FileAccess.Write), System.Drawing.Imaging.ImageFormat.Png);
+                        }
+                    }
+                    duplicated.Save();
+                }
+
+                this.filledTemplates.Add(duplicated);
+                this.filledTemplateStreams.Add(stream);
+                //document.InsertDocument(duplicated);
+            }
+
+        }
+
+        //https://www.codeproject.com/articles/187928/imageupdater
+        
+        public void UpdateImage(Dictionary<string, MemoryStream> tagValueDict)
+        {
+            foreach (var pair in tagValueDict)
+            {
+                var tagID = pair.Key;
+                var imageStream = pair.Value;
+
+                //foreach (SdtElement sdtElement in _mainDocPart.Document.Body.Descendants<SdtElement>())
+                //{
+                //    string relID = GetImageRelID(sdtElement, tagID);
+                //    if (!string.IsNullOrEmpty(relID))
+                //    {
+                //        // Get size of image
+                //        int imageWidth;
+                //        int imageHeight;
+                //        GetPlaceholderImageSize(_mainDocPart.Document.Body.Descendants<Drawing>(), relID, out imageWidth, out imageHeight);
+
+                //        UpdateImagePart(relID, imageStream, imageWidth, imageHeight);
+
+                //        break;
+                //    }
+                //}
+            }
+        }
+
+
+        //上面的代码用于在某个 sdt 元素下面查找匹配内容控件ID所使用的图像资源id。然后我们根据该资源id来获取placeholder image的大小：
+        internal static void GetPlaceholderImageSize(IEnumerable<Drawing> drawingList, string relID, out int width, out int height)
+        {
+            width = -1;
+            height = -1;
+
+            // Loop through all Drawing elements in the document
+            foreach (Drawing d in drawingList)
+            {
+                // Loop through all the pictures (Blip) in the document
+                //if (d.Descendants<Blip>().ToList().Any(b => b.Embed.ToString() == relID))
+                //{
+                //    // The document size is in EMU. 1 pixel = 9525 EMU
+
+                //    // The size of the image placeholder is located in the EXTENT element
+                //    Extent e = d.Descendants<Extent>().FirstOrDefault();
+                //    if (null != e)
+                //    {
+                //        width = (int)(e.Cx / 9525);
+                //        height = (int)(e.Cy / 9525);
+                //    }
+
+                //    if (width == -1)
+                //    {
+                //        // The size of the image is located in the EXTENTS element
+                //        Extents e2 = d.Descendants<Extents>().FirstOrDefault();
+                //        if (null != e2)
+                //        {
+                //            width = (int)(e2.Cx / 9525);
+                //            height = (int)(e2.Cy / 9525);
+                //        }
+                //    }
+                //}
+            }
+        }
+        //获取到大小信息之后，我们就可以使用资源id以及图像大小信息，替换图像来替换具体的placeholder图像了。
+        private void UpdateImagePart(string relID, MemoryStream imageStream, int width, int height)
+        {
+            //var originalBitmap = Image.FromStream(imageStream);
+            //var bitmap = originalBitmap;
+            //// resize image
+            //if (width != -1)
+            //{
+            //    bitmap = new Bitmap(originalBitmap, width, height);
+            //}
+
+            //// Save image data to ImagePart
+            //var stream = new MemoryStream();
+            //bitmap.Save(stream, originalBitmap.RawFormat);
+
+            //// Get the ImagePart
+            //var imagePart = (ImagePart)_mainDocPart.GetPartById(relID);
+
+            //// Create a writer to the ImagePart
+            //var writer = new BinaryWriter(imagePart.GetStream());
+
+            //// Overwrite the current image in the docx file package
+            //writer.Write(stream.ToArray());
+
+            //// Close the ImagePart
+            //writer.Close();
+        }
+    }
+}
---
+++ DCTS/UI/EditCustomerTripDayForm.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace DCTS.UI
+{
+    partial class EditCustomerTripDayForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tripDayFormControl1 = new DCTS.UI.TripDayFormControl();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.entityDataSource1 = new DCTS.CustomComponents.EntityDataSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.No;
+            this.cancelButton.Location = new System.Drawing.Point(434, 357);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(75, 23);
+            this.cancelButton.TabIndex = 13;
+            this.cancelButton.Text = "取消";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            // 
+            // saveButton
+            // 
+            this.saveButton.DialogResult = System.Windows.Forms.DialogResult.Yes;
+            this.saveButton.Location = new System.Drawing.Point(353, 357);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 14;
+            this.saveButton.Text = "保存";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(-48, 120);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 12);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "备注";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(-48, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "名称";
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(1, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(587, 41);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "编辑某天详细";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(-48, 91);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 12);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "天数";
+            // 
+            // tripDayFormControl1
+            // 
+            this.tripDayFormControl1.Location = new System.Drawing.Point(27, 61);
+            this.tripDayFormControl1.Name = "tripDayFormControl1";
+            this.tripDayFormControl1.Size = new System.Drawing.Size(507, 290);
+            this.tripDayFormControl1.TabIndex = 15;
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // entityDataSource1
+            // 
+            this.entityDataSource1.DbContextType = typeof(DCTS.DctsEntities);
+            // 
+            // EditCustomerTripDayForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(587, 400);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tripDayFormControl1);
+            this.Name = "EditCustomerTripDayForm";
+            this.Text = "编辑某天详细";
+            this.Load += new System.EventHandler(this.EditTripForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.Label label4;
+        private CustomComponents.EntityDataSource entityDataSource1;
+        private TripDayFormControl tripDayFormControl1;
+    }
+}
