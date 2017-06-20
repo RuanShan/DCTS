769c898017911f42d82a452a5ebe6e196bf63f3d
qinghe <areq22@gmail.com>
2017-06-20 20:51:17 +0800

    添加setup.sql

--- DCTS/Sql/setup.sql
+++ DCTS/Sql/setup.sql
@@ -23,7 +23,7 @@ DROP TABLE IF EXISTS `Cities`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `Cities` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `id` int(10) NOT NULL AUTO_INCREMENT,
   `code` varchar(24) DEFAULT '',
   `nationId` int(10) unsigned NOT NULL DEFAULT '0',
   `nationCode` varchar(12) NOT NULL DEFAULT '',
@@ -32,8 +32,9 @@ CREATE TABLE `Cities` (
   `position` int(11) NOT NULL DEFAULT '0',
   `enabled` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=586 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=637 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
 
 
 --
@@ -74,7 +75,7 @@ CREATE TABLE `ComboLocations` (
   `word` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `ltype` (`ltype`)
-) ENGINE=InnoDB AUTO_INCREMENT=910 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=1261 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 
@@ -92,11 +93,12 @@ CREATE TABLE `Customers` (
   `birthday` datetime DEFAULT NULL COMMENT '生日',
   `enname` varchar(128) DEFAULT NULL COMMENT '英文姓名',
   `passport` varchar(45) DEFAULT NULL COMMENT '护照号码',
-  `created_at` datetime DEFAULT NULL,
+  `created_at` datetime NOT NULL,
   `updated_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
 
 
 --
@@ -120,81 +122,32 @@ CREATE TABLE `DayLocations` (
   `cover_id` int(11) NOT NULL DEFAULT '0' COMMENT '封面图片',
   `schedule` varchar(2048) DEFAULT NULL COMMENT '行程计划\n无法仅仅使用每天行程作为每一项, 比如, 仅仅有景点浏览开始时间,没有离开酒店的出发时间.\n或者何时去机场,火车站等.这是一个自定义项  时间||活动//时间||活动',
   `flight_no` varchar(45) DEFAULT NULL,
-  `flight_from` varchar(45) DEFAULT NULL,
-  `flight_to` varchar(45) DEFAULT NULL,
-  `flight_layover` tinyint(4) NOT NULL DEFAULT '0',
-  `flight_start_at` datetime DEFAULT NULL,
-  `flight_end_at` datetime DEFAULT NULL,
+  `flight_from` varchar(45) DEFAULT NULL COMMENT '飞机出发地点/提车地点',
+  `flight_to` varchar(45) DEFAULT NULL COMMENT '飞机到达地/还车地点',
+  `flight_layover` tinyint(1) NOT NULL DEFAULT '0',
+  `flight_start_at` datetime DEFAULT NULL COMMENT '飞机出发时间/提车时间',
+  `flight_end_at` datetime DEFAULT NULL COMMENT '航班到达时间/还车时间',
   `flight2_start_at` datetime DEFAULT NULL,
   `flight2_end_at` datetime DEFAULT NULL,
   `flight2_from` varchar(45) DEFAULT NULL,
   `flight2_to` varchar(45) DEFAULT NULL,
   `flight2_no` varchar(45) DEFAULT NULL,
-  `airport_from` varchar(45) DEFAULT NULL,
-  `airport_to` varchar(45) DEFAULT NULL,
+  `airport_from` varchar(45) DEFAULT NULL COMMENT '飞机出发机场/提车详细地址',
+  `airport_to` varchar(45) DEFAULT NULL COMMENT '航班到达机场/还车详细地址',
   `airport2_from` varchar(45) DEFAULT NULL,
   `airport2_to` varchar(45) DEFAULT NULL,
+  `flight_ticket` varchar(45) DEFAULT NULL COMMENT '租车凭证号',
+  `flight_company` int(11) NOT NULL DEFAULT '0',
+  `tips` varchar(1024) DEFAULT NULL,
+  `memo` varchar(1024) DEFAULT NULL,
+  `route` varchar(1024) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `trip_id_idx` (`trip_id`),
   KEY `day_id_idx` (`day_id`),
   CONSTRAINT `fk_day_id` FOREIGN KEY (`day_id`) REFERENCES `TripDays` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=136 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=146 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
-
-
---
--- Table structure for table `Dinings`
---
-
-DROP TABLE IF EXISTS `Dinings`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `Dinings` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `nation` varchar(45) DEFAULT NULL,
-  `city` varchar(45) DEFAULT NULL,
-  `area` varchar(45) DEFAULT NULL,
-  `title` varchar(45) DEFAULT NULL,
-  `dishes` varchar(45) DEFAULT NULL,
-  `img` varchar(45) DEFAULT NULL,
-  `latlng` varchar(45) DEFAULT NULL,
-  `reach` varchar(45) DEFAULT NULL,
-  `address` varchar(45) DEFAULT NULL,
-  `recommendedDishes` varchar(45) DEFAULT NULL,
-  `tips` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
-
---
--- Table structure for table `Hotels`
---
-
-DROP TABLE IF EXISTS `Hotels`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `Hotels` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `nation` varchar(45) DEFAULT NULL,
-  `city` varchar(45) DEFAULT NULL,
-  `title` varchar(45) DEFAULT NULL,
-  `localTitle` varchar(45) DEFAULT NULL,
-  `img` varchar(45) DEFAULT NULL,
-  `room` varchar(45) DEFAULT NULL,
-  `dinner` varchar(45) DEFAULT NULL,
-  `address` varchar(45) DEFAULT NULL,
-  `latlng` varchar(45) DEFAULT NULL,
-  `contact` varchar(45) DEFAULT NULL,
-  `wifi` varchar(45) DEFAULT NULL,
-  `parking` varchar(45) DEFAULT NULL,
-  `reception` varchar(45) DEFAULT NULL,
-  `kitchen` varchar(45) DEFAULT NULL,
-  `tips` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 
 --
@@ -207,13 +160,14 @@ DROP TABLE IF EXISTS `LocationImages`;
 CREATE TABLE `LocationImages` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `location_id` int(11) NOT NULL,
-  `name` varchar(45) NOT NULL COMMENT '文件名',
+  `name` varchar(128) NOT NULL COMMENT '文件名',
   `memo` varchar(128) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_location_idx` (`location_id`),
   CONSTRAINT `fk_location` FOREIGN KEY (`location_id`) REFERENCES `ComboLocations` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
 
 
 --
@@ -224,7 +178,7 @@ DROP TABLE IF EXISTS `Nations`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `Nations` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `id` int(10) NOT NULL AUTO_INCREMENT,
   `code` varchar(12) DEFAULT '',
   `title` varchar(45) NOT NULL DEFAULT '',
   `localTitle` varchar(45) DEFAULT '',
@@ -234,30 +188,118 @@ CREATE TABLE `Nations` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 
+
 --
--- Table structure for table `Scenics`
+-- Table structure for table `Schedules`
 --
 
-DROP TABLE IF EXISTS `Scenics`;
+DROP TABLE IF EXISTS `Schedules`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `Scenics` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `nation` varchar(45) DEFAULT '',
-  `city` varchar(45) DEFAULT NULL,
-  `title` varchar(1024) DEFAULT NULL,
-  `localTitle` varchar(45) DEFAULT NULL,
-  `img` varchar(45) DEFAULT NULL,
-  `latlng` varchar(45) DEFAULT NULL,
-  `localAdd` varchar(45) DEFAULT NULL,
-  `openAt` datetime DEFAULT NULL,
-  `closeAt` datetime DEFAULT NULL,
-  `ticket` varchar(45) DEFAULT NULL,
-  `tips` varchar(1024) CHARACTER SET latin1 DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+CREATE TABLE `Schedules` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `tripday_id` int(11) NOT NULL DEFAULT '0',
+  `start_at` datetime DEFAULT NULL,
+  `end_at` datetime DEFAULT NULL,
+  `title` varchar(45) DEFAULT NULL,
+  `desc` varchar(1024) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_tripday_idx` (`tripday_id`),
+  CONSTRAINT `fk_tripday` FOREIGN KEY (`tripday_id`) REFERENCES `TripDays` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
+
+
+--
+-- Table structure for table `Suppliers`
+--
+
+DROP TABLE IF EXISTS `Suppliers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Suppliers` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `stype` int(11) NOT NULL DEFAULT '0',
+  `name` varchar(45) DEFAULT NULL,
+  `img` varchar(45) DEFAULT NULL,
+  `csh` varchar(45) DEFAULT NULL,
+  `memo` varchar(1024) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `baggage_weight_limit` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+
+--
+-- Table structure for table `Tickets`
+--
+
+DROP TABLE IF EXISTS `Tickets`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Tickets` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `daylocation_id` int(11) NOT NULL DEFAULT '0',
+  `customer_id` int(11) NOT NULL,
+  `supplier_id` int(11) NOT NULL,
+  `trip_id` int(11) NOT NULL,
+  `ttype` int(11) NOT NULL DEFAULT '0' COMMENT '为了快速找到某一类型的票',
+  `title` varchar(45) DEFAULT NULL,
+  `num` varchar(45) DEFAULT NULL,
+  `route` varchar(1024) DEFAULT NULL,
+  `memo` varchar(1024) DEFAULT NULL,
+  `requirement` varchar(1024) DEFAULT NULL,
+  `service_no` varchar(45) DEFAULT NULL,
+  `overlay` tinyint(1) DEFAULT NULL,
+  `overlay_sign` int(11) NOT NULL DEFAULT '0',
+  `start_at` datetime DEFAULT NULL,
+  `end_at` datetime DEFAULT NULL,
+  `from_place` varchar(45) DEFAULT NULL,
+  `to_place` varchar(45) DEFAULT NULL,
+  `from_location_id` int(11) NOT NULL DEFAULT '0',
+  `to_location_id` int(11) NOT NULL DEFAULT '0',
+  `rules` varchar(1024) DEFAULT NULL,
+  `days` int(11) NOT NULL DEFAULT '0',
+  `city` varchar(45) DEFAULT NULL,
+  `breakfirst` varchar(45) DEFAULT NULL,
+  `parking` varchar(45) DEFAULT NULL,
+  `baggage_weight_limit` varchar(45) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_customer_idx` (`customer_id`),
+  KEY `fk_supplier_idx` (`supplier_id`),
+  KEY `fk_ticket_trip_idx` (`trip_id`),
+  CONSTRAINT `fk_customer` FOREIGN KEY (`customer_id`) REFERENCES `Customers` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_supplier` FOREIGN KEY (`supplier_id`) REFERENCES `Suppliers` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_ticket_trip` FOREIGN KEY (`trip_id`) REFERENCES `Trips` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+
+--
+-- Table structure for table `TripCustomers`
+--
+
+DROP TABLE IF EXISTS `TripCustomers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `TripCustomers` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `trip_id` int(11) NOT NULL DEFAULT '0',
+  `customer_id` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `fk_customer_idx` (`customer_id`),
+  KEY `fk_trip_idx` (`trip_id`),
+  CONSTRAINT `fk_tripcustomer_customer` FOREIGN KEY (`customer_id`) REFERENCES `Customers` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_tripcustomer_trip` FOREIGN KEY (`trip_id`) REFERENCES `Trips` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 
 --
@@ -278,8 +320,9 @@ CREATE TABLE `TripDays` (
   `cover_id` int(11) NOT NULL DEFAULT '0' COMMENT '封面图片',
   PRIMARY KEY (`id`),
   KEY `tripdays_idx` (`trip_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=80 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=109 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
 
 
 --
@@ -299,11 +342,13 @@ CREATE TABLE `Trips` (
   `start_at` datetime DEFAULT NULL COMMENT '行程开始时间',
   `end_at` datetime DEFAULT NULL COMMENT '行程结束时间',
   `cover_id` int(11) NOT NULL DEFAULT '0' COMMENT '封面图片',
+  `countries` varchar(256) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`),
   KEY `fk_customer_idx` (`customer_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
 
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -315,4 +360,4 @@ CREATE TABLE `Trips` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed
+-- Dump completed on 2017-06-20 17:30:59
--- DCTS/UI/SuppliersControl.Designer.cs
+++ DCTS/UI/SuppliersControl.Designer.cs
@@ -709,6 +709,8 @@
             // 
             // groupBox1
             // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.groupBox1.Location = new System.Drawing.Point(10, 46);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(799, 55);
